a02c8ef0db09eeb9fa4ddf09f57c8cf0428dedc5
Jack <john9872@stthomas.edu>
2026-02-12 19:36:28 -0600

    Done

--- pointers.cpp
+++ pointers.cpp
@@ -1,11 +1,12 @@
 // compile: g++ -std=c++14 -o pointers pointers.cpp
 #include <iostream>
 #include <string>
+#include <cmath>
 
 typedef struct Student {
     int id;
-    char *f_name;
-    char *l_name;
+    std::string f_name;
+    std::string l_name;
     int n_assignments;
     double *grades;
 } Student;
@@ -19,11 +20,26 @@ int main(int argc, char **argv)
     Student student;
     double average;
 
-    // Sequence of user input -> store in fields of `student`
-
-    // Call `CalculateStudentAverage(???, ???)`
-    // Output `average`
-
+    student.id = promptInt("Please enter the student's id number: ", 0, 999999999);
+    std::cout << "Please enter the student's first name: ";
+    std::cin >> student.f_name;
+    std::cout << "Please enter the student's last name: ";
+    std::cin >> student.l_name;
+    student.n_assignments = promptInt("Please enter how many assignments were graded: ", 1, 134217728);
+    std::cout << std::endl;
+    student.grades = new double[student.n_assignments];
+    for(int i = 0; i < student.n_assignments; i++){
+        std::string m = "Please enter the grade for assignment " + std::to_string(i);
+        m += ": ";
+        student.grades[i] = promptDouble(m, 0, 1000);
+    }
+    std::string info = "Student: " + student.f_name + " " + student.l_name + " [" + std::to_string(student.id) + "]";
+    double *grades2 = new double[student.n_assignments + 1];
+    grades2[0] = student.n_assignments;
+    for(int i = 0; i < student.n_assignments; i++){
+        grades2[i+1] = student.grades[i];
+    }
+    calculateStudentAverage(&info, grades2);
     return 0;
 }
 
@@ -34,7 +50,22 @@ int main(int argc, char **argv)
 */
 int promptInt(std::string message, int min, int max)
 {
-    // Code to prompt user for an int
+    int toReturn;
+    bool inRange = true;
+    std::cout << message;
+    std::cin >> toReturn;
+    if(toReturn > max || toReturn < min){
+        inRange = false;
+    }
+    while(!inRange){
+        std::cout << "Sorry, I cannot understand your answer" << std::endl;
+        std::cout << message;
+        std::cin >> toReturn;
+        if(toReturn < max && toReturn > min){
+            inRange = true;
+        }
+    }
+    return toReturn;
 }
 
 /*
@@ -43,8 +74,23 @@ int promptInt(std::string message, int min, int max)
    max: maximum value to accept as a valid double
 */
 double promptDouble(std::string message, double min, double max)
-{
-    // Code to prompt user for a double
+{  
+    double toReturn;
+    bool inRange = true;
+    std::cout << message;
+    std::cin >> toReturn;
+    if(toReturn > max || toReturn < min){
+        inRange = false;
+    }
+    while(!inRange){
+        std::cout << "Sorry, I cannot understand your answer" << std::endl;
+        std::cout << message;
+        std::cin >> toReturn;
+        if(toReturn < max && toReturn > min){
+            inRange = true;
+        }
+    }
+    return toReturn;
 }
 
 /*
@@ -53,5 +99,16 @@ double promptDouble(std::string message, double min, double max)
 */
 void calculateStudentAverage(void *object, double *avg)
 {
-    // Code to calculate and store average grade
+    std::string& s = *static_cast<std::string*>(object);
+    std::cout << s << std::endl;
+    double a = 0.0;
+    for(int i = 0; i < avg[0]; i++){
+        a += avg[i+1];
+    }
+    a /= avg[0];
+    a = a*10;
+    a = std::round(a);
+    int b = static_cast<int>(a) % 10;
+    a = a / 10;
+    std::cout << "  Average grade: " + std::to_string(static_cast<int>(a)) + "." + std::to_string(b);
 }
